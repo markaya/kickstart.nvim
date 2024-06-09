4cb07def1b15af1267908981dccd3593ea79ff4b
markaya <markoristic15@gmail.com>
2024-06-09 07:47:32 +0200

    Remove markdown

--- lua/markaya/plugins/todo_comments.lua
+++ lua/markaya/plugins/todo_comments.lua
@@ -1,3 +1,6 @@
 return {
-  { 'folke/todo-comments.nvim', event = 'vimenter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  'folke/todo-comments.nvim',
+  event = 'vimenter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false }
 }
